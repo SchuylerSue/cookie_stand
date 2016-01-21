ad812e5653e9fb446218754497a7ba7e49f0c814
SchuylerSue <schuylergrahamjs@msn.com>
2016-01-20 21:10:12 -0800

    update index.html app.js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var pikePlace = {
   maximumCustomers: 88,
   averageCookies: 5.2,
 };
+pikePlace.generateRandom = function(min, max){
+return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(pikePlace.minimumCustomers)
+console.log(pikePlace.generateRandom(pikePlace.minimumCustomers, pikePlace.maximumCustomers));
