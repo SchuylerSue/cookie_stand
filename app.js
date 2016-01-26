c433a041b23b59ae1490240c24d400e270c6eca4
SchuylerSue <schuylergrahamjs@msn.com>
2016-01-25 18:52:55 -0800

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 var pikePlace = {
   minimumCustomers: 17,
   maximumCustomers: 88,
   averageCookies: 5.2,
-};
-pikePlace.generateRandom = function(min, max){
-return Math.floor(Math.random() * (max - min + 1) + min);
+  hourlySales: [],
+  totalSales: 0,
 }
-console.log(pikePlace.minimumCustomers)
-console.log(pikePlace.generateRandom(pikePlace.minimumCustomers, pikePlace.maximumCustomers));
+  pikePlace.generateRandomcust = function(){
+    return Math.random() * (pikePlace.maximumCustomers - pikePlace.minimumCustomers) + pikePlace.minimumCustomers;
+}
+  pikePlace.generateaverageCookies = function(){
+    return Math.random() * pikePlace.averageCookies + pikePlace.hourlySales
+  }
+// console.log(pikePlace.minimumCustomers)
+// console.log(pikePlace.generateRandom(pikePlace.minimumCustomers, pikePlace.maximumCustomers));
+//
+//   pikePlace.generateaverageCookies: function () {
+//   return Math.floor(Math)
+//   pikePlace.gethourlySales: function () {
+//   return Math.floor(Math)
+// }
+// for (var i = 0; i < pikePlace.length; i++){
+//   + pikePlace (minimumCustomers [i], maximumCustomers [i], averageCookies [i], hourlySales [i], totalSales [i]))
+// }
+// document.getElementByID()
+// document.createTextContent()
